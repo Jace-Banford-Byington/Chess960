e021242f97b9a206759f58b71e5c0f536fc07db5
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2023-11-14 21:29:52 -0700

    Moved files back in fully

---
+++ ChessGame/js/960.js
@@ -0,0 +1,14 @@
+function randomizeBackRank(){
+    //Generate random 960 setup
+    //Ensure bishop are on opposite-colored squares
+    //update piece creation
+
+};
+
+function restBoard(){
+    //rest everything on the page
+    //call RandomizeBack to generate new layout
+    //update board and pieces
+};
+
+randomizeBackRank();
